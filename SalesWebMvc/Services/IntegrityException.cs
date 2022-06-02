6b70c7e0e10f086d7d63abeb2b4dbed525368c7d
Abrahão Neto <abrahao-neto@outlook.com>
2022-06-01 21:07:59 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc/Services/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services
+{
+    public class IntegrityException : AccessViolationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
